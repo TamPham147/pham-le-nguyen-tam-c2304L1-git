6e5e976a0d1e9ecf56a1e1d11abffa6dfce29be4
NGUYENTAM <lenguyentam.pham@gmail.com>
2023-11-23 20:31:44 +0700

    bai4

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-//bai2
-function tinhGiaTriBieuThuc(x, y, z) {
-    return 3 * x**2 - 5 * x * y + z**4;
-}
-
-// Kiểm tra hàm với x = 5, y = 1, z = 2
-const ketQua = tinhGiaTriBieuThuc(5, 1, 2);
-console.log("Kết quả:", ketQua);
-
-
-//bai3
+//bai 4
 const arr = [5, 1, 4, 2, 7];
+let sum = 0;
 
-console.log("Danh sách các phần tử của mảng:");
 for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+    sum += arr[i];
 }
 
+console.log("Tổng các phần tử trong mảng là:", sum);
