46635d22c18cb1200c344d855fe77a9ba7fa2169
NGUYENTAM <lenguyentam.pham@gmail.com>
2023-11-23 20:23:23 +0700

    bai2

---
+++ index.js
@@ -0,0 +1,27 @@
+//bai2
+function tinhGiaTriBieuThuc(x, y, z) {
+    return 3 * x**2 - 5 * x * y + z**4;
+}
+
+// Kiểm tra hàm với x = 5, y = 1, z = 2
+const ketQua = tinhGiaTriBieuThuc(5, 1, 2);
+console.log("Kết quả:", ketQua);
+
+
+//bai3
+const arr = [5, 1, 4, 2, 7];
+
+console.log("Danh sách các phần tử của mảng:");
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+//bai 4
+const arr = [5, 1, 4, 2, 7];
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+
+console.log("Tổng các phần tử trong mảng là:", sum);
